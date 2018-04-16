5fe4c395212434101a4b6356ddd755ff66efba09
mizobaraya <mazenguitar@gmail.com>
2018-04-16 22:16:15 +0300

    Face detection from live camera

---
+++ face_dec_live.py
@@ -0,0 +1,20 @@
+import dlib
+from skimage.io import imread
+from imutils import face_utils as facee
+import cv2 as cv
+detector = dlib.get_frontal_face_detector()
+#pose_detect = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+cap = cv.VideoCapture(0)
+while True:
+    ret, img = cap.read()
+    faces = detector(img, 2)
+    if faces:
+
+        for face in faces:
+
+
+            x, y, w, h = facee.rect_to_bb(face)
+            cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv.imshow("Frame", img)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
